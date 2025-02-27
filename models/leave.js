685585fb70f92224e0f20c88c248b8f0b3a371f8
Xuewu Huang <huangxuewu@gmail.com>
2025-02-26 23:50:42 -0500

    *add leave

---
+++ models/leave.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const database = require("../config/database");
+
+const leaveSchema = new mongoose.Schema({
+    employee: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "employee",
+        required: true
+    },
+    leaveType: {
+        type: String,
+        enum: ["Sick", "Vacation", "Unpaid"],
+        required: true
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["Pending", "Approved", "Rejected"],
+        default: "Pending"
+    },
+    reason: {
+        type: String,
+        required: true
+    },
+    approvedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "employee",
+        default: null
+    },
+    approvedAt: {
+        type: Date,
+        default: null
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true });
+
+module.exports = database.model("leave", leaveSchema, "leave");
