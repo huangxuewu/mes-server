4ef477f57a1b36ad5c2cd5607ff93168cdc49b0a
huangxuewu <huangxuewu@gmail.com>
2025-04-09 10:42:09 -0400

    update server config

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { io, app, server } = require("./socket/io");
 const socketHandler = require("./socket/index");
 
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "0.0.0.0";
+
 io.on("connection", (socket) => socketHandler(socket, io));
 
 app.get("/", (req, res) => {
