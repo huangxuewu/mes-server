04d0c274dde0d78dd2f38771ea630753cf9b3a7f
Xuewu Huang <huangxuewu@gmail.com>
2026-06-14 02:20:19 -0400

    *add modify permission

--- models/user.js
+++ models/user.js
@@ -20,6 +20,8 @@ const userSchema = new mongoose.Schema({
         access: [String],
         create: [String],
         view: [String],
+        update: [String],
+        modify: [String],
         edit: [String],
         delete: [String],
         approve: [String],
